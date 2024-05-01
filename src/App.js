6b6545fe8facb1db0c3653ecb9060ff6dde6aac6
goodperiodt <ilypsj@gmail.com>
2024-05-01 16:24:11 +0900

    react 초기 설정

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const $h2 = <h2>반가워요~~~</h2>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className='App'>
+        <h1>메롱메롱</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type='text'/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 5월 1일 수요일입니다. <br />
+          오후 3시 30분입니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
