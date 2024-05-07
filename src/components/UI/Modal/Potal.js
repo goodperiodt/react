a2340a95286980a18e7463ad38773e0f476e795b
goodperiodt <ilypsj@gmail.com>
2024-05-07 11:59:49 +0900

    SideEffect템플릿 생성 및 문제상황 확인

---
+++ src/components/UI/Modal/Potal.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+const Potal = ({ children: renderComponent, destId }) => {
+  return ReactDOM.createPortal(
+    renderComponent,
+    document.getElementById(destId),
+  );
+};
+
+export default Potal;
