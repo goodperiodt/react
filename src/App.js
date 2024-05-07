37c15b1e4b4c716d06d38e00431584115a53eca0
goodperiodt <ilypsj@gmail.com>
2024-05-07 12:41:22 +0900

    useEffect 사용으로 로그인 유저 한번만 확인함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import AddUsers from './components/Users/AddUsers';
 // import UserList from './components/Users/UserList';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
@@ -12,11 +12,15 @@ const App = () => {
   // 화면이 리렌더링 될 때 LocalStorage를 확인해서
   // 현재 Login-flag가 존재하는지 검사
   console.log('로그인 검사 수행');
-  const storedLoginFlag = localStorage.getItem('login-flag');
-  if (storedLoginFlag === '1') {
-    setIsLoggedIn(true);
-  }
-
+  // 기존에 로그인 한 사람인지 확인하는 코드는
+  // 리렌더링 될 때마다 실행하면 안된다.(한번만 확인하면 됨)
+  useEffect(() => {
+    console.log('useEffect 실행 - 최초 단 한번만 실행됨');
+    const storedLoginFlag = localStorage.getItem('login-flag');
+    if (storedLoginFlag === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
   // 서버로 로그인을 요청하는 함수, (나중에는 fetch를 통한 백엔드와의 연계가 필요하다.)
   const loginHandler = (email, password) => {
     // 로그인을 했다는 증거로 상태값 변경 및 브라우저에 로그인 값을 1로 표현해서 저장
@@ -24,9 +28,14 @@ const App = () => {
     localStorage.setItem('login-flag', '1'); // LocalStorage: 브라우저에서 제공하는 저장소
     setIsLoggedIn(true);
   };
+
+  const logoutHandler = ({ isAuthenticated, onLogout }) => {
+    localStorage.removeItem('login-flag');
+    setIsLoggedIn(false);
+  };
   return (
     <div>
-      <MainHeader />
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
